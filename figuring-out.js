8c4bc708e401448ce16b599ac36f76162981dd22
Amy <amybbzxx512@googlemail.com>
2016-07-04 07:56:06 -0500

    yj

--- figuring-out.js
+++ figuring-out.js
@@ -1,10 +1,11 @@
 //Converter Class 
 var Converter = require("csvtojson").Converter;
-var converter = new Converter({});
+var converter = new Converter({headers:["fact"]}) ;
  
 //end_parsed will be emitted once parsing finished 
 converter.on("end_parsed", function (jsonArray) {
-   console.log(jsonArray); //here is your result jsonarray 
+    var index = Math.floor(Math.random() * jsonArray.length)
+   console.log(jsonArray[index]); //here is your result jsonarray 
 });
  
 //read from file 
