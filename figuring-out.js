7ffee4d03fdbab60e075e90d5275777fa3afb8e6
Amy <amybbzxx512@googlemail.com>
2016-07-04 07:41:44 -0500

    dh

--- figuring-out.js
+++ figuring-out.js
@@ -1,1 +1,11 @@
 var fs = require('fs')
+var filename = process.argv[3]
+    fs.readFile(filename, function(e, data) {
+        if (e) return res.sendStatus(500)
+        try {
+            books = JSON.parse(data)
+        } catch (e) {
+            res.sendStatus(500)
+        }
+        res.json(books)
+    })
